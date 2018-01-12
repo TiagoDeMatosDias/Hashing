a6d871dddf0960373874f367fb0ce627d08d6cd1
poloport <poloport@users.noreply.github.com>
2018-01-12 12:26:46 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Hashing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetHash(Console.ReadLine()));
+            Console.ReadLine();
+        }
+
+        public static string GetHash(string input)
+        {
+            HashAlgorithm algorithm = SHA256.Create();
+            return BitConverter.ToString(algorithm.ComputeHash(Encoding.UTF8.GetBytes(input))).Replace("-", String.Empty);
+        }
+    }
+}
